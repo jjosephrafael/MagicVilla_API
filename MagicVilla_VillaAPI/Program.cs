d5f610a3e1bd66d227e8d5a6c450c3caa5d941d8
Joseph Rafael Javate <jjosephrafael@gmail.com>
2024-01-07 03:44:42 -0500

    Section 3: CRUD Functionality Http Patch Nuget Packages

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// add .AddNewtonsoftJson(); to builder.Services.AddControllers() to register the nuget package added
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
